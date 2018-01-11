26491c0777c4e60a0f3c784345786a36bdd738b6
luckpalohiya007 <luckypalohiya@gmail.com>
2018-01-11 19:38:37 +0530

    Add files via upload

---
+++ .gitignore/H.java
@@ -0,0 +1,12 @@
+class H 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main begin");
+		int[] x = new int[9999999999999];
+		System.out.println("main begin");
+	}
+}
+/*
+outofmemoryerror  
+in java heap memory*/
