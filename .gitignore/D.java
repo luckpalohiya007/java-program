543421702709409e2ac4ac9a5c84eba193b6be90
luckpalohiya007 <luckypalohiya@gmail.com>
2018-01-11 19:34:07 +0530

    Add files via upload

--- .gitignore/D.java
+++ .gitignore/D.java
@@ -1,23 +1,14 @@
-//reverse fibonacci series
 class D
 {
 	public static void main(String[] args) 
 	{
-		int i = 0, j = 1;
-		int k = 0;
-		while ((k = i + j) < 10)
-		{
-			i = j;
-			j = k;
-		}
-		System.out.println("fib series in the reverse:");
-		System.out.print(j + ", ");
-		while(i > 0)
-		{
-			i = k - j;
-			System.out.print(i + ", ");
-			k = j;
-			j = i;
-		}
+		System.out.println("main begin");
+		int[] x = {10, 20, 30};
+		int i = x[9];
+		System.out.println("main end");
 	}
 }
+/*
+main begin 
+ArrayIndexOutOfBoundsException
+*/
