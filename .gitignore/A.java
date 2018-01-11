20ff96da9c3c7f8c4b8f841b3b7c9b51d735ee41
luckpalohiya007 <luckypalohiya@gmail.com>
2018-01-11 19:27:34 +0530

    Add files via upload

--- .gitignore/A.java
+++ .gitignore/A.java
@@ -1,18 +1,13 @@
-class A
+class A 
 {
 	public static void main(String[] args) 
 	{
-		int i = 0, j = 1;
-		System.out.println("fib series:");
-		System.out.print(i + ", " + j + ", ");
-		int k = 0;
-		while (k < 100)
-		{
-			k = i + j;
-			System.out.print(k + ", ");
-			i = j;
-			j = k;
-		}
-
+		System.out.println("main begin");
+		int i = 10 / 0;//flow terminated at line no 6
+		System.out.println("main end");
 	}
 }
+/*
+main begin
+Arithmatic exception
+*/
